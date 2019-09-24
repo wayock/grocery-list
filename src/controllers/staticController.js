07b53613d128ac4d0c1b0e99f02b726960086ac6
Matthew Wayock <wayock@gmail.com>
2019-09-24 15:43:52 -0400

    controller update

--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -1,5 +1,5 @@
 module.exports = {
-  index(req, res, next){
-     res.render("static/index", {title: "Welcome to the Grocery List"});
+  index(req, res, next) {
+    res.render("static/index", { title: "Welcome to the Grocery List" });
   }
-}
+};
